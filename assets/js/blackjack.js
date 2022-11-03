6540f6d04a2ab656b98bf9d9f55567ae7295452e
tWilltp <williamjenkinsfeb@gmail.com>
2022-11-03 10:44:49 +0000

    finished and stored blackjack game with other js exmples on blackjack.js file, began writing chrome extension

--- assets/js/blackjack.js
+++ assets/js/blackjack.js
@@ -1,3 +1,79 @@
+// entire blackjack game
+
+let cards = []
+let sum = 0
+let blackJack = false
+let isAlive = false
+let message = ""
+let messageEl = document.getElementById('message-el')
+let sumEl = document.getElementById('sum-el')
+let cardsEl = document.getElementById('cards-el')
+
+let player = {
+    chips: 100,
+    name: "Bill"
+}
+
+let playerEl = document.getElementById('player-el')
+playerEl.textContent = player.name + ", " + player.chips
+
+function getRandomCard() {
+
+    let randomNumber = Math.floor(Math.random() * 13) + 1
+    if (randomNumber > 10) {
+        return 10
+    } else if (randomNumber === 1) {
+        return 11
+    } else {
+        return randomNumber
+    }
+
+}
+
+function startGame() {
+
+    let fCard = getRandomCard()
+    let sCard = getRandomCard()
+    cards = [fCard, sCard]
+    sum = fCard + sCard
+    isAlive = true
+    renderGame()
+    
+}
+
+function renderGame() {
+
+    cardsEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + ", "
+    }
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = "New card?"
+    } else if (sum === 21) {
+        message = "BlackJack!"
+        blackJack = true
+    } else {
+        message = "Bust"
+        isAlive = false
+    }
+    messageEl.textContent = "Dealer: " + message
+
+}
+
+function newCard() {
+
+    if (isAlive === true && blackJack === false) {
+
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        renderGame()
+
+    }
+
+}
+
 // basic race game returning fastest time, hard coded
 
 let player1Time = 102
@@ -134,3 +210,22 @@ function RPS() {
 }
 
 console.log(RPS())
+
+// sorting through arrays
+
+let fruit = [1, 2, 1, 1, 2]
+let appleShelf = document.getElementById("apple-shelf")
+let orangeShelf = document.getElementById("orange-shelf")
+
+function sortFruit() {
+    for (let i = 0; i < fruit.length; i += 1) {
+        if (fruit[i] === 1) {
+            appleShelf.textContent += 1
+        } else if (fruit[1] === 2) {
+            orangeShelf.textContent += 2
+        }
+    }
+    
+}
+
+sortFruit()
